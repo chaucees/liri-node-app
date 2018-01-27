6a38f62372c44eb42e633f2152fdcaf29d8e956b
Chaucee <hi@chaucee.com>
2018-01-27 10:07:24 -0700

    more file set up

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'mNE4MK45OLY8krRpKCudsLr90',
+  consumer_secret: 'DKeAk3XfXivEAHOapUt2iO02KtRmFrCDlvsuBm4VunJvyaIYYJ',
+  access_token_key: '22718320-R0DUibS3AN0qnEi6ltsy1EELBOaj2vOZtkY9Nppvg',
+  access_token_secret: 'qPKt3idifnNjI0kU73zq95OBYNFFYaCb1fWi5qRdLnMlA',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,8 @@
+// At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
+// Make it so liri.js can take in one of the following commands:
+
+
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
